68635bb058b9afc906b03809eb8038d1252c7280
Robin Eklind <rnd0x00@gmail.com>
2018-10-07 02:04:40 +0200

    lbg: use gotool instead of buildutil.ExpandPatterns as work-around
    Until golang/go#25543 gets fixed.

--- cmd/lbg/parse.go
+++ cmd/lbg/parse.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"go/build"
 	"log"
 	"os"
@@ -9,10 +8,10 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/kisielk/gotool"
 	"github.com/mewkiz/pkg/term"
 	"github.com/mewmew/lbg/internal/syntax"
 	"github.com/pkg/errors"
-	"golang.org/x/tools/go/buildutil"
 )
 
 var (
@@ -23,19 +22,33 @@ var (
 
 // Parse parses the Go packages specified by the given patterns.
 func Parse(patterns []string) (map[string]*Package, error) {
+	// Expand patterns into Go package paths.
 	dbg1.Println("patterns:")
 	for _, pattern := range patterns {
 		dbg1.Printf("   %v", pattern)
 	}
 	p := NewParser(&build.Default)
-	pkgPaths, err := expandPatterns(p.ctxt, patterns)
+	gotool.DefaultContext.BuildContext = *p.ctxt
+	pkgPaths := gotool.ImportPaths(patterns)
+	sort.Strings(pkgPaths)
+	dbg1.Println("package paths:")
+	for _, pkgPath := range pkgPaths {
+		dbg1.Printf("   %v", pkgPath)
+	}
+	pkgPaths, err := fixRelPaths(p.ctxt, pkgPaths)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
+	dbg1.Println("package paths (cannonical):")
+	for _, pkgPath := range pkgPaths {
+		dbg1.Printf("   %v", pkgPath)
+	}
+
+	// Add packages to parse queue.
 	for _, pkgPath := range pkgPaths {
 		p.push(Elem{PkgPath: pkgPath})
 	}
-	// Parse pseudo-package builtin for predeclared identifiers.
+	// Add builtin pseudo-package to parse queue for predeclared identifiers.
 	p.push(Elem{PkgPath: "builtin"})
 	if err := p.Parse(); err != nil {
 		return nil, errors.WithStack(err)
@@ -102,78 +115,27 @@ func (p *Parser) push(elem Elem) {
 
 // ### [ Helper functions ] ####################################################
 
-// expandPatterns returns the Go package paths specified by the given patterns.
-func expandPatterns(ctxt *build.Context, patterns []string) ([]string, error) {
-	// Note, relative patterns (e.g. "." and "../") are not yet well supported by
-	// buildutil.ExpandPatterns. There is a TODO in that package to extend
-	// support, but for now, we use our own implementation.
-	ps, err := fixPatterns(ctxt, patterns)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	dbg1.Println("patterns (fixed):")
-	for _, p := range ps {
-		dbg1.Printf("   %v", p)
-	}
-	m := buildutil.ExpandPatterns(ctxt, ps)
+// fixRelPaths returns the cannonical package import paths for the (potentially)
+// relative package import paths.
+func fixRelPaths(ctxt *build.Context, relPkgPaths []string) ([]string, error) {
 	var pkgPaths []string
-	for pkgPath := range m {
-		pkgPaths = append(pkgPaths, pkgPath)
-	}
-	sort.Strings(pkgPaths)
-	dbg1.Println("package paths:")
-	for _, pkgPath := range pkgPaths {
-		dbg1.Printf("   %v", pkgPath)
-	}
-	return pkgPaths, nil
-}
-
-// TODO: remove fixPatterns once buildutil.ExpandPatterns handles relative
-// patterns.
-
-// fixPatterns translates relative import patterns to qualified import patterns.
-func fixPatterns(ctxt *build.Context, patterns []string) ([]string, error) {
-	var ps []string
-	for _, pattern := range patterns {
-		switch {
-		// Relative to current directory.
-		case pattern == "." || strings.HasPrefix(pattern, "./"):
-			absPath, err := filepath.Abs(".")
+	for _, relPkgPath := range relPkgPaths {
+		if strings.HasPrefix(relPkgPath, ".") {
+			absPkgPath, err := filepath.Abs(relPkgPath)
 			if err != nil {
 				return nil, errors.WithStack(err)
 			}
-			pkgPath, err := findPkgPath(ctxt, absPath)
+			pkgPath, err := findPkgPath(ctxt, absPkgPath)
 			if err != nil {
 				return nil, errors.WithStack(err)
 			}
-			p := fmt.Sprintf("%s%s", pkgPath, pattern[len("."):])
-			ps = append(ps, p)
-		// Relative to parent directory.
-		case pattern == ".." || strings.HasPrefix(pattern, "../"):
-			p := pattern
-			parents := 0
-			for ; strings.HasPrefix(p, "../"); p = p[len("../"):] {
-				parents++
-			}
-			if p == ".." {
-				parents++
-				p = ""
-			}
-			absPath, err := filepath.Abs(strings.Repeat("../", parents))
-			if err != nil {
-				return nil, errors.WithStack(err)
-			}
-			pkgPath, err := findPkgPath(ctxt, absPath)
-			if err != nil {
-				return nil, errors.WithStack(err)
-			}
-			ps = append(ps, strings.Join([]string{pkgPath, p}, "/"))
-		// Use pattern as is.
-		default:
-			ps = append(ps, pattern)
+			pkgPaths = append(pkgPaths, pkgPath)
+		} else {
+			pkgPaths = append(pkgPaths, relPkgPath)
 		}
 	}
-	return ps, nil
+	sort.Strings(pkgPaths)
+	return pkgPaths, nil
 }
 
 // findPkgPath returns the qualified package path (as relative to a Go src
